aaf8701c4277d599368114f8707e81dc6a45b80e
javadocq <han18264@naver.com>
2026-03-09 18:10:21 +0900

    Add SQLite Package

--- obj/Debug/net10.0-windows/DesktopContactsApp.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/DesktopContactsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesktopContactsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8dbe6e82a4669a25553ba4478c6219dbccee05")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesktopContactsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesktopContactsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
