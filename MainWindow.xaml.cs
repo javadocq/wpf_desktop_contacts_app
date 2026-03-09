5e8dbe6e82a4669a25553ba4478c6219dbccee05
javadocq <han18264@naver.com>
2026-03-09 17:53:23 +0900

    Initial DesktopContacts App

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewContactWindow newContactWindow = new NewContactWindow();
+            // newContactWindow.Show(); // 창이 열려 있어도 다른 창에서 작업 가능
+            newContactWindow.ShowDialog(); // 해당 창이 닫히기 전까지는 다른 창에서 작업 불가능
+        }
+    }
+}
