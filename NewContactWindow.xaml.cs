c23681f74e4a952fe8ab9f321d59bac8eb9b06d4
javadocq <han18264@naver.com>
2026-03-09 20:18:06 +0900

    Connect SQLite

--- NewContactWindow.xaml.cs
+++ NewContactWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SQLite;
+using DesktopContactsApp.Classes;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -24,6 +26,25 @@ namespace DesktopContactsApp
 
         private void saveButton_Click(object sender, RoutedEventArgs e)
         {
+            Contact contact = new Contact() { 
+                Name = nameTextBox.Text,
+                Email = emailTextBox.Text,
+                Phone = phoneTextBox.Text
+            };
+            string databaseName = "Contacts.db";
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments); // 윈도우의 '내 문서' 폴더 경로를 자동으로 찾아준다.
+            string databasePath = System.IO.Path.Combine(folderPath, databaseName); // C:\Users\이름\Documents\Contacts.db와 같은 전체 주소를 만든다.
+
+            //SQLiteConnection connection = new SQLiteConnection(databasePath); // 지정한 경로의 DB 파일을 연다.
+            //connection.CreateTable<Contact>(); // 이미 있다면 이 구문은 무시된다.
+            //connection.Insert(contact); // 저장
+            //connection.Close(); // 닫지 않으면 다른 곳에서 이 파일을 열 수 없다.
+
+            using (SQLiteConnection connection = new SQLiteConnection(databasePath)) {
+                connection.CreateTable<Contact>(); // 이미 있다면 이 구문은 무시된다.
+                connection.Insert(contact); // 저장
+            } // using 블록이 끝나면 자동으로 Close 호출
+
             Close(); // 현재 창 닫기
         }
     }
--- obj/Debug/net10.0-windows/DesktopContactsApp.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/DesktopContactsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesktopContactsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8dbe6e82a4669a25553ba4478c6219dbccee05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaf8701c4277d599368114f8707e81dc6a45b80e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesktopContactsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesktopContactsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
